707c32281c50be05b6527cf1b13a1153c191c776
Edwin Troya <edtroyajr@gmail.com>
2021-04-03 14:23:03 -0700

    compare files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
    <Router>
