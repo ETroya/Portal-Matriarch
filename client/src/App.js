5ca45bebe39ddf5d8c1ea3d4e9450e696c24f852
Edwin Troya <edtroyajr@gmail.com>
2021-04-04 14:02:34 -0700

    config e.s lint

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
    <Router>
