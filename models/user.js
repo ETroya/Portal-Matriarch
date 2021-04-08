daa018c212757502338a71a52d99584a0b5b3d58
amar <amarmann92@gmail.com>
2021-04-08 16:18:00 -0700

    working on user creation

--- models/user.js
+++ models/user.js
@@ -1,22 +1,20 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema= new mongoose.Schema({
-    email:{
-        type: String, 
-        required: false,
-        unique: true,
-        },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-    },
-    password:{
-        type: String,
-        required: true,
-    }
-})
-
-module.exports= User= mongoose.model("user",UserSchema)
-
+module.exports = User = mongoose.model("user", UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,33 @@
-const router= require("express").Router()
-const bcrypt= require("bcyrptjs")
-const User= require("../../models")
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const User = require("../../models/user");
 
-router.post("/", async(req, res)=> {
-    const {username, password}= req.body
-    try {
-        const user= new User({
-            username, password
-        })
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = new User({
+      username,
+      password,
+    });
 
-        const salt= await bcrypt.genSalt(10);
-        user.password= await bcrypt.hash(password, salt);
 
-        await user.save()
-    } catch (error) {
-        res.status(500).json({message: "Server error try again!"})
-    }
-})
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
 
-router.get("/", async(req, res)=> {
-    try {
-        const users= await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({message: "Server error try again!"})
-    }
-})
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error try again!" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error try again!" });
+  }
+});
+
+module.exports = router;
