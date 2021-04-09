0aba9328c938ec4da4b26d6408bbb63d2fde1aa1
amar <amarmann92@gmail.com>
2021-04-08 19:55:23 -0700

    updated userjs

--- models/user.js
+++ models/user.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: false,
-    unique: true,
-  },
   username: {
     type: String,
     required: true,
