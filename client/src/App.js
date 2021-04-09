9d05af1f22cc8e3d4a2d958c3d1b3380777ce0a0
Marlon Matthew <mbm.drums@gmail.com>
2021-04-08 22:34:03 -0700

    Changed color scheme of nav bar.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 import User from "./pages/user";
 import Login from "./pages/login";
 import Admin from "./pages/admin";
@@ -11,27 +10,24 @@ import { StateProvider } from "./utils/GlobalState";
 function App() {
   return (
     <StateProvider>
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path={["/user", "/"]}>
-            <h1>hello user</h1>
-            <User />
-          </Route>
-          <Route exact path={"/admin"}>
-            <Admin />
-          </Route>
-          <Route path= "/login" component= {Login} />
-          <Route path= "/signup" component= {Signup} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container-fluid">
+            <Route exact path={["/user", "/"]}>
+              <h1>hello user</h1>
+              <User />
+            </Route>
+            <Route exact path={"/admin"}>
+              <Admin />
+            </Route>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </StateProvider>
-
-
-
   );
 }
 
