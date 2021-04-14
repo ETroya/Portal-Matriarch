51687dd6d12f00557e25d51a3bfa67e7fb6b3455
Carl <cfuer12@yahoo.com>
2021-04-13 18:07:07 -0700

    minimal changes

---
+++ client/src/components/Footer/index 2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>input footer here</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/nextDay/index 2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const index = () => {
+  return (
+    <div className="sched">
+      <h1>Tomorrows Sched.</h1>
+      <h2>Start Time: 6 am</h2>
+    </div>
+  );
+};
+
+export default index;
