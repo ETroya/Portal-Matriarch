d831101ebb90d89ccf22c9d1f7ce1ba526b16431
Carl <cfuer12@yahoo.com>
2021-04-17 10:49:11 -0700

    made usernames present in list instead of actual names

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,72 +1,92 @@
 import React from "react";
 
 // creating list for workers to pull from api and show on site
-function List({ firstName, lastName, id}) {
+function List({ firstName, lastName, id, username }) {
   return (
-      <tr className = "drag-item" value= {id} draggable ondragstart="event.dataTransfer.setData('text/plain',null)">
-        <td className="first-name">
-          {firstName}
-        </td>
-        <td className="last-name">
-          {lastName}
-        </td>
-      </tr>
+    <tr
+      className="drag-item"
+      value={id}
+      draggable
+      ondragstart="event.dataTransfer.setData('text/plain',null)"
+    >
+      <td className="user-name">{username}</td>
+      {/* <td className="first-name">{firstName}</td>
+      <td className="last-name">{lastName}</td> */}
+    </tr>
   );
 }
 
 //drag and drop function
 var dragged;
 
-  /* events fired on the draggable target */
-  document.addEventListener("drag", function( event ) {
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {}, false);
 
-  }, false);
-
-  document.addEventListener("dragstart", function( event ) {
+document.addEventListener(
+  "dragstart",
+  function (event) {
     // store a ref. on the dragged elem
     dragged = event.target;
     // make it half transparent
-    event.target.style.opacity = .5;
-      
-  }, false);
+    event.target.style.opacity = 0.5;
+  },
+  false
+);
 
-  document.addEventListener("dragend", function( event ) {
-      // reset the transparency
-      event.target.style.opacity = "";
-  }, false);
+document.addEventListener(
+  "dragend",
+  function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+  },
+  false
+);
 
-  /* events fired on the drop targets */
-  document.addEventListener("dragover", function( event ) {
-      // prevent default to allow drop
-      event.preventDefault();
-  }, false);
+/* events fired on the drop targets */
+document.addEventListener(
+  "dragover",
+  function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  },
+  false
+);
 
-  document.addEventListener("dragenter", function( event ) {
-      // highlight potential drop target when the draggable element enters it
-      if ( event.target.className === "dropzone" ) {
-          event.target.style.background = "purple";
-      }
+document.addEventListener(
+  "dragenter",
+  function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className === "dropzone") {
+      event.target.style.background = "purple";
+    }
+  },
+  false
+);
 
-  }, false);
+document.addEventListener(
+  "dragleave",
+  function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className === "dropzone") {
+      event.target.style.background = "";
+    }
+  },
+  false
+);
 
-  document.addEventListener("dragleave", function( event ) {
-      // reset background of potential drop target when the draggable element leaves it
-      if ( event.target.className === "dropzone" ) {
-          event.target.style.background = "";
-      }
-
-  }, false);
-
-  document.addEventListener("drop", function( event ) {
-    console.log(event)
-      // prevent default action (open as link for some elements)
-      event.preventDefault();
-      // move dragged elem to the selected drop target
-      if ( event.target.className === "dropzone" ) {
-          event.target.style.background = "";
-          dragged.parentNode.removeChild( dragged );
-          event.target.appendChild( dragged );
-      }
-    
-  }, false);
+document.addEventListener(
+  "drop",
+  function (event) {
+    console.log(event);
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className === "dropzone") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+    }
+  },
+  false
+);
 export default List;
