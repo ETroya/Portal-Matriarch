b8378b448b3e22aeee3f84ab13581049562cdc39
Marlon Matthew <mbm.drums@gmail.com>
2021-04-17 19:01:33 -0700

    went through older commits to fix and bring back carlos changes from earlier today... went through erics and everything seems fine but posting doesnt seem to work

--- client/src/components/comment_list/index.js
+++ client/src/components/comment_list/index.js
@@ -1,16 +1,19 @@
-//maps over all the comments to display as a list 
+//maps over all the comments to display as a list
 import IndividualComment from "../individualComment/index";
 import "./style.css";
 
 const CommentList = ({ comments }) => {
-
   return (
     <div className="comment-list">
       {comments.map((comment) => {
-
+        console.log(comment);
         return (
           <div className="comment-card">
-            <IndividualComment content={comment.content} id={comment.id} author={comment.author} />
+            <IndividualComment
+              content={comment.content}
+              id={comment.id}
+              author={comment.author}
+            />
           </div>
         );
       })}
