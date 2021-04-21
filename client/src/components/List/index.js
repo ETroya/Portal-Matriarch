b58077ea5a8ea6898b51ed5539ffab53db580b3e
Carl <cfuer12@yahoo.com>
2021-04-20 21:41:29 -0700

    made a file finder for time

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -9,15 +9,9 @@ function List({ firstName, lastName, id}) {
       // draggable
       // ondragstart="event.dataTransfer.setData('text/plain',null)"
     >
-<<<<<<< HEAD
-      {/* <td className="user-name text-light">{username}</td> */}
-      <td className="first-name text-light">{firstName}</td>
-      <td className="last-name text-light">{lastName}</td>
-=======
       <td className="first-name text-light">{firstName}</td>
       <td className="last-name text-light">{lastName}</td>
     
->>>>>>> 3a9f68d4cbdd083a944e7cb4324b82670a1ff351
     </tr>
   );
 }
--- client/src/components/Time/index.js
+++ client/src/components/Time/index.js
@@ -1,4 +1,18 @@
+import React from "react";
+import { Form, Jumbotron } from "react-bootstrap";
+
 const Time = () => {
-  return <div>Time</div>;
+  return <div>
+     <Jumbotron fluid>
+      <Form.Group>
+        <Form.Label for="exampleFile">Submit time-off sheet</Form.Label>
+        <Form.Control type="file" name="file" id="exampleFile" />
+        <Form.Text color="muted">
+          This is some placeholder block-level help text for the above input.
+          It's a bit lighter and easily wraps to a new line.
+        </Form.Text>
+      </Form.Group>
+    </Jumbotron>
+  </div>;
 };
 export default Time;
